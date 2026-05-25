88f77d6cb4b71a0825c363cdb8a56f2de8e49832
Nata616 <natanaelriosutn@gmail.com>
2026-05-25 20:14:13 -0300

    se realizo la funcion cargar_array y calcular promedio, falta documentar

--- array_unidimensional.py
+++ array_unidimensional.py
@@ -1,2 +1,31 @@
-def crear_array(cantidad):
+def crear_array(cantidad:int) -> int:
+    """
+    _summary_
+
+    Args:
+        cantidad (int): _description_
+
+    Returns:
+        int: _description_
+    """
+
     return[0] * cantidad
+
+def cargar_array(cantidad:int) -> list:
+    """
+    _summary_
+
+    Args:
+        cantidad (int): _description_
+
+    Returns:
+        list: _description_
+    """
+
+    mi_array = crear_array(cantidad)
+    for i in range(cantidad):
+        mi_array[i] = int(input(f"Ingrese el numero para la posicion [{i}]: "))
+    return mi_array
+
+def calcular_promedio(lista:list) -> float:
+    pass
